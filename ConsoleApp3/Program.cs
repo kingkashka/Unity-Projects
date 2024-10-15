3fa982ca3628b280a388039b99edbd914bb06283
Nicholas Keyes <148286779+kingkashka@users.noreply.github.com>
2024-10-15 14:27:29 -0500

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DateTime yearofBirth = new DateTime(1995, 2, 22, 23, 30, 45);
+        DateTime yearofGraduation = new DateTime(2013, 6, 1, 16, 34, 22);
+        DateTime timeNow = DateTime.Now;
+
+        TimeSpan ageAtGraduation = yearofGraduation - yearofBirth;
+        TimeSpan ageNow = timeNow - yearofBirth;
+        int timeinMinutes = ageNow.Minutes;
+        Console.WriteLine(ageNow);
+
+
+        Console.WriteLine("Input any number");
+        string number = Console.ReadLine();
+
+        File.WriteAllText(@"C:\Users\keyes\Desktop\log.txt", number);
+
+        //This gets rid and erases memory
+        using (StreamWriter file = new StreamWriter(@"C:\Users\keyes\Desktop\log.txt", true))
+        {
+            file.WriteLine(number);
+            file.WriteLine(timeNow);
+        }
+
+        List<string> names = new List<string>();
+        names.Add("Benjamin Button");
+        names.Add("Hotel Moon");
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
